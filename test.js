e7ff6bdbb067f851b4b0149dd02c43e345bc0895
Jesse Tane <jesse.tane@gmail.com>
2015-11-28 18:17:18 -0500

    test for oob

--- test.js
+++ test.js
@@ -1,13 +1,45 @@
 var tape = require('tape')
 var blockFromCodePoint = require('./')
 
-tape('it works', function (t) {
-  t.plan(1)
+tape('get first block', function (t) {
+  t.plan(2)
+  t.deepEqual(blockFromCodePoint(0), {
+    name: 'Basic Latin',
+    start: 0x0000,
+    end: 0x007F
+  })
+  t.deepEqual(blockFromCodePoint(0x7F), {
+    name: 'Basic Latin',
+    start: 0x0000,
+    end: 0x007F
+  })
+})
 
-  var block = blockFromCodePoint(0xF0100)
-  t.deepEqual(block, {
-    name: 'Supplementary Private Use Area-A',
-    start: 0xF0000,
-    end: 0xFFFFF
+tape('get last block', function (t) {
+  t.plan(2)
+  t.deepEqual(blockFromCodePoint(0x100000), {
+    name: 'Supplementary Private Use Area-B',
+    start: 0x100000,
+    end: 0x10FFFF
+  })
+  t.deepEqual(blockFromCodePoint(0x10FFFF), {
+    name: 'Supplementary Private Use Area-B',
+    start: 0x100000,
+    end: 0x10FFFF
+  })
+})
+
+tape('get non-unicode code points', function (t) {
+  t.plan(2)
+  t.equal(blockFromCodePoint(-1), undefined)
+  t.equal(blockFromCodePoint(0x110000), undefined)
+})
+
+tape('get the fire engine\'s block', function (t) {
+  t.plan(1)
+  t.deepEqual(blockFromCodePoint(0x1F692), {
+    name: 'Transport and Map Symbols',
+    start: 0x1F680,
+    end: 0x1F6FF
   })
 })
